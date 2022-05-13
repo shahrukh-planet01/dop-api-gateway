e5a1a1aeb450579f78d38d26149e3889c6273fb0
shahrukh-planet01 <shahrukh.khan@planet01.net>
2022-05-13 20:22:43 +0500

    Initial Commit

---
+++ src/main/java/net/planet01/dopapigateway/DopApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package net.planet01.dopapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DopApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/planet01/dopapigateway/DopApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.planet01.dopapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
